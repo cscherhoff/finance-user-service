abd3943e4b25b228b8e7ba9beb026a52d57b2280
cscherho <cscherhoff@web.de>
2022-09-04 14:17:46 +0200

    Add logging to TransactionController

--- src/main/java/com/exxeta/userservice/controller/investment/TransactionController.java
+++ src/main/java/com/exxeta/userservice/controller/investment/TransactionController.java
@@ -3,6 +3,8 @@ package com.exxeta.userservice.controller.investment;
 import com.exxeta.userservice.InvestmentServiceProxy;
 import com.exxeta.userservice.config.CustomJwtAuthenticationFilter;
 import com.exxeta.userservice.model.investment.TransactionDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,8 @@ public class TransactionController {
     private final InvestmentServiceProxy investmentServiceProxy;
     private final CustomJwtAuthenticationFilter filter;
 
+    private final Logger logger = LoggerFactory.getLogger(TransactionController.class);
+
     public TransactionController(InvestmentServiceProxy investmentServiceProxy, CustomJwtAuthenticationFilter filter) {
         this.investmentServiceProxy = investmentServiceProxy;
         this.filter = filter;
@@ -23,13 +27,16 @@ public class TransactionController {
     @PostMapping(path = "/transaction")
     @ResponseStatus(HttpStatus.CREATED)
     public String addNewTransaction(@Valid @RequestBody TransactionDto transaction) {
+        logger.info("Received transaction: " + transaction);
         long userId = filter.getUserIdFromToken();
+        logger.info("Using user ID " + userId);
         return investmentServiceProxy.addNewTransaction(userId, transaction);
     }
 
     @GetMapping(path = "/transactions")
     public String getAllTransactions() {
         long userId = filter.getUserIdFromToken();
+        logger.info("Getting all transactions for user ID " + userId);
         return investmentServiceProxy.getAllTransactions(userId);
     }
 }
